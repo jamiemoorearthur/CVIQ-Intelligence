a376a5d6868df42935f7a58b7fe7e6aed19ae4de
Jamie Moore-Arthur <Jamiemoorearthur@gmail.com>
2026-07-08 23:19:45 +0100

    Update BASE_URL to use double quotes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://cvreview-api.duckdns.org'
+const BASE_URL = "https://cvreview-api.duckdns.org/";
 
 export const reviewCV = async (cvFile, jobDescription) => {
   const formData = new FormData()
